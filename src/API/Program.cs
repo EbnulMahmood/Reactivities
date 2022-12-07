9612fe171f1c216c9f959ebcd6a8edc6417e8aa5
shovan <shovonmahmood67@gmail.com>
2022-12-07 18:06:54 +0600

    add dbcontext

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Presistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +16,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+try
+{
+    app.Services
+        .GetRequiredService<DataContext>()
+        .Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
